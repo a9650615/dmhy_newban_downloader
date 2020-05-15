633d3fd953e1c1a4ea5e167638edef3836de7691
Ming You <a9650615@gmail.com>
2020-05-16 02:46:36 +0800

    Auto schedule to refresh the list

--- app/index.js
+++ app/index.js
@@ -1,3 +1,4 @@
+import Schedule from 'node-schedule'
 import NewBanDatabase from '../db/NewBanDatabase'
 import TaskDatabase from '../db/TaskDatabase'
 import GoogleDriveDatabase from '../db/GoogleDriveDatabase'
@@ -7,18 +8,25 @@ import UploadGD from '../lib/UploadGD'
 
 // Flow: 新番列表 -> 查詢資料 -> 下載 -> 下載列表
 
-const app = async () => {
-  await NewBanDatabase.init()
-  await TaskDatabase.init()
-  await GoogleDriveDatabase.init()
-  await UploadGD.init()
-  DownloadManager.start()
+const updateList = () => {
+  log.debug('自動排程啟動')
   updateNewListOfDay.subscribe((data) => {
     // console.log(data)
     getTodayUpdateList.subscribe((data) => {
   
     })
   })
+}
+
+const app = async () => {
+  await NewBanDatabase.init()
+  await TaskDatabase.init()
+  await GoogleDriveDatabase.init()
+  await UploadGD.init()
+  DownloadManager.start()
+
+  Schedule.scheduleJob('0 5 */8 * * *', updateList)
+  updateList()
 }
 // UpdateNewListOfDay.subscribe(app)
 
